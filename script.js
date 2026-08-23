3604530f4fd6484f68c68606634249825acf0315
Andi Fauzi <andifauziathallahdzaki@gmail.com>
2026-08-23 18:56:00 +0800

    Commit 3

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // Navigasi Mobile Menu (Hamburger Toggle)
-const menuToggle = document.getElementById('menuToggle');
-const navLinks = document.getElementById('navLinks');
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
 });
 
 // Tutup menu otomatis setelah klik link (untuk mode mobile)
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', () => {
-        navLinks.classList.remove('active');
+document.querySelectorAll(".nav-links a").forEach((link) => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
     });
+});
+
+// Typewriter Effect
+document.addEventListener("DOMContentLoaded", () => {
+    const option = {
+        strings: [
+            "Xcelsior SMANSA 2026.",
+            "Angkatan 67.",
+            "Generasi Unggul dan Kreatif.",
+        ],
+
+        typeSpeed: 60,
+        backSpeed: 40,
+        backDelay: 1500,
+        loop: true,
+
+        showCursor: true,
+        cursorChar: "_",
+        autoInsertCss: true,
+    };
+
+    const typed = new Typed("#typed-text", option);
 });
